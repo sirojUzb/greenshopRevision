3cb99cf66abbcd5566d45b8f83e11e835e288a67
sirojUzb <sirojiddinmamatovuzb@gmail.com>
2024-04-26 04:51:33 +0900

    navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import ReactDOM from "react-dom/client";
 import "./style.css";
 import App from "./App";
-import Home from "./Components/Home";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
     <App />
-    <Home />
   </div>
 );
